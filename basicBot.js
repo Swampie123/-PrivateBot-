683a30ef5353de27468267df15996f027f0bae50
Swampie123 <gaming1238@inbox.lv>
2015-04-05 12:37:56 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/Swampie123/-PrivateBot-/master/basicBot.js", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
